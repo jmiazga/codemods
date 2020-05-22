aeceba5b78fbd31cf87b71d4ad6a34c85d9772a3
jmiazga <jmiazga@gmail.com>
2020-05-22 17:01:46 -0400

    codemod to rename a chakra button's variantColor prop to colorScheme

---
+++ Component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@chakra-ui/core";
+
+const Component = ({ props }) => {
+  return (
+    <>
+      <Button variantColor="teal" />
+    </>
+  );
+};
+
+export default Component;
---
+++ README.md
@@ -0,0 +1,5 @@
+to run codemod:
+jscodeshift -t variantColor-to-colorScheme.ts Component.js -d -p
+
+using this for inspiration:
+https://skovy.dev/jscodeshift-custom-transform/
